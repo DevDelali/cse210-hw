42f81803ebfbb6ca7c226061032c8b20749e22f8
Paul Delali <delalpaul5@gmail>
2025-09-04 13:01:45 +0000

    display name of user after input

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,5 +5,11 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+
+        Console.Write("Please enter your first name :");
+        string first_name = Console.ReadLine().ToUpper(); // capitalizing the first name
+        Console.Write("Please enter your last name :");
+        string last_name = Console.ReadLine().ToUpper();  // capitalizing the last name
+        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
     }
-}
+}      
