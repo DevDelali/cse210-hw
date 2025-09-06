f4929d62d0843d957e50fbe86671238440b95b8e
Paul Delali <delalpaul5@gmail>
2025-09-06 16:17:16 +0000

    completed exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -6,10 +6,15 @@ class Program
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
 
-        Console.Write("What is the magic number ?: ");
-        int magicNumber = int.Parse(Console.ReadLine());
+        // Console.Write("What is the magic number ?: ");
+        // int magicNumber = int.Parse(Console.ReadLine());
+        
+        Random random = new Random();
 
-        int guessNumber = -1; // Initialize guessNumber to a value that can't be equal to magicNumber
+        int magicNumber = random.Next(101);
+        Console.WriteLine("Random integer (0 to 100)");
+
+        int guessNumber = -1;
 
         while (guessNumber != magicNumber)
         {
