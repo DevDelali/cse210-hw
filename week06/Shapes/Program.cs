69b444cca4f00fa84d2c0437c20b529b113615af
Paul Delali <delalpaul5@gmail>
2025-10-10 15:05:03 +0000

    completed learning activity on polymorphism

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -5,5 +5,15 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Shapes Project.");
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(new Circle(5.0, "Green"));
+        shapes.Add(new Square(4.0, "Red"));
+        shapes.Add(new Circle(2.5, "Blue"));
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"The  {shape.GetColor()} shape has an area of {shape.GetArea():.00}\n");
+        }
     }
 }
